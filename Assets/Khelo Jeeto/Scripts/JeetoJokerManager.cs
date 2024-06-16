720a7b4562c7feaddd1e8aad57f908ed662091b7
Sudhanshu <dev.sks96@gmail.com>
2024-06-16 18:46:48 +0530

    [Update-KheloJeeto]: Multiplier added, minor ui changes and fixes.

--- Assets/Khelo Jeeto/Scripts/JeetoJokerManager.cs
+++ Assets/Khelo Jeeto/Scripts/JeetoJokerManager.cs
@@ -37,6 +37,7 @@ namespace KheloJeeto
 		[SerializeField] private Sprite heartSprite, spadeSprite, diamondSprite, clubSprite;
 		[SerializeField] private Image[] betHistoryCardRanks;
 		[SerializeField] private Image[] betHistoryCardSuits;
+		[SerializeField] private Text[] betHistoryMultiplier;
 		[SerializeField] private EachCard[] eachCards;
 		[SerializeField] private Button clearButton, doubleButton, repeatButton;
 		[SerializeField] private Button allHeartsButton, allSpadesButton, allDiamondsButton, allClubsButton;
@@ -199,6 +200,15 @@ namespace KheloJeeto
 					if (i == 0)
 					{
 						timer.SetLastDrawTime(mainData.lastFewDrawDetails.Draws[i].DrawTime);
+					}
+
+					string xFVal = mainData.lastFewDrawDetails.Draws[i].XF;
+					if (xFVal == "N" || xFVal.ToLower() == "1x")
+						betHistoryMultiplier[i].transform.parent.gameObject.SetActive(false);
+					else
+					{
+						betHistoryMultiplier[i].transform.parent.gameObject.SetActive(true);
+						betHistoryMultiplier[i].text = xFVal.ToLower();
 					}
 
 					string cardRankAndSuit = GetCardRankAndSuitAccordingToNumber(mainData.lastFewDrawDetails.Draws[i].Result);
